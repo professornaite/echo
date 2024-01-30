e403933c96923dc20d4bf7338c1f8342a517605a
professornaite <professornaite@gmail.com>
2024-01-30 13:58:13 -0500

    source

--- code/01_data_processing (seda).R
+++ code/01_data_processing (seda).R
@@ -1,4 +1,6 @@
 # step 01: data processing (seda)
 
+# data source: https://edopportunity.org/get-the-data/seda-archive-downloads/
+
 seda2022 <- read.csv("data/seda2022_admindist_poolsub_gys_2.0.csv")
 seda2022
